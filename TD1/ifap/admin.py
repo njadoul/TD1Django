fc796909222feac9f129bb0f43f30272186ebfa6
njadoul <nathanjadoul@live.fr>
2018-10-04 01:03:47 +0200

    Ajout d'example dans la base de données plus commit final

--- TD1/ifap/admin.py
+++ TD1/ifap/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 
+from .models import Classe
+from .models import Ordre
+from .models import Famille
+from .models import Animal
+
 admin.site.register(Classe)
 admin.site.register(Ordre)
 admin.site.register(Famille)
