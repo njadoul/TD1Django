a770bc8ea23552e8f57e34366c417081c3179ed9
njadoul <nathanjadoul@live.fr>
2018-10-04 00:45:24 +0200

    Création des models

--- TD1/ifap/models.py
+++ TD1/ifap/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+
+class Classe(models.Model):
+    classe_name = models.CharField(max_length = 30)
+    
+    def _str_(slef):
+        return self.classe
+
+class Ordre(models.Model):
+    classe = models.ForeignKey(Classe, on_delete = models.CASCADE)
+    ordre_name = models.CharField(max_length = 30)
+
+    def _str_(slef):
+        return self.ordre
+
+class Famille(models.Model):
+    ordre = models.ForeignKey(Ordre, on_delete = models.CASCADE)
+    famille_name = models.CharField(max_length = 30)
+
+    def _str_(slef):
+        return self.famille
+
+class Animal(models.Model):
+    famille = models.ForeignKey(Famille, on_delete = models.CASCADE)
+    cummun_name = models.CharField(max_length = 50)
+    sc_name = models.CharField(max_length = 50)
+
+    def _str_(slef):
+        return self.animal
+
+        
